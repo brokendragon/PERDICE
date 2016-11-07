b2ea00e1c1793a18a8425ea092b27ba30baaa715
huangwanyu0810 <huangwanyu0810@gmail.com>
2016-11-07 09:58:31 +0800

    change the score.py

--- PERDICE/fuzz/score.py
+++ PERDICE/fuzz/score.py
@@ -1,20 +1,25 @@
+# -*- coding: UTF-8 -*-
+#   This file is part of Fuzzgrind.
+#   Copyright (C) 2009 Gabriel Campana
+#
+#   This work is licensed under the terms of the GNU GPL, version 3.
+#   See the LICENSE file in the top-level directory.
+
+
 import os
 import random
 import re
 import subprocess
 import sys
-
+import time
+import cPickle as pickle
 
 FUZZGRIND = './valgrind-r12356/build/bin/valgrind'
 
 
-def random_score():
-    return random.randint(0,1000000)
-    
-    
+
 def score(progname, progarg, input_file, taint_stdin=False):
-    
-    arg_valgrind = [ FUZZGRIND, '--tool=cachegrind', '--branch-sim=yes' ]
+    arg_valgrind = [ FUZZGRIND, '--tool=lackey', '--basic-counts=yes']
     arg_prog = [ progname, input_file ]
     
     if not progarg:
@@ -25,43 +30,67 @@ def score(progname, progarg, input_file, taint_stdin=False):
         progarg = re.sub('\$input', input_file, progarg)
         progarg = progarg.split()
         arg_prog = [ progname ] + progarg
-
+    
     if not taint_stdin:
         stdin = None
     else:
         stdin = open(input_file, 'r')
-    
+
     p = subprocess.Popen(arg_valgrind + arg_prog, stdin=stdin, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     stdout, stderr = p.communicate()
-
-    line_score = {}
-
-    for lines in stderr.split('*****'):
-        
-        for line in lines.split('\n'):
-            m1 = re.match('file:\s+(.*)', line)
-            m2 = re.match('==\d+==\s+line:\s+([\d,]+)', line)
-            m3 = re.match('==\d+==\s+I1\s+misses:\s+([\d,]+)', line)
-            m4 = re.match('==\d+==\s+D1\s+misses:\s+([\d,]+)', line)
-            m5 = re.match('==\d+==\s+LL misses:\s+([\d,]+)', line)
-            m6 = re.match('==\d+==\s+Mispredicts:\s+([\d,]+)', line)
-            if m1:
-                file = m1.group(1)
-            if m2:
-                linenum = int(re.sub(',', '', m2.group(1)))
-            if m3:
-                value1 = int(re.sub(',', '', m3.group(1)))
-            if m4:
-                value2 = int(re.sub(',', '', m4.group(1)))
-            if m5:
-                value3 = int(re.sub(',', '', m5.group(1)))
-            if m6:
-                value4 = int(re.sub(',', '', m6.group(1)))
-                break
- 
-        score = (value1+value2)*1+value3*20+value4*3
-        line_score[(file,linenum)] = score
+    total = 0
+    for line in stderr.split('\n'):
+        m = re.match('==\d+==\s+total:\s+([\d,]+)', line)
+        if m:
+            total = int(  re.sub(',', '', m.group(1) ) )
+            break
+    #print total
     
-    return line_score
+    return total
+	
+	
+def scoreMs(progname, progarg, input_file, taint_stdin=False):
+    
+    arg_valgrind = [ FUZZGRIND, '--tool=massif', '--massif-out-file=massif-output']
+    arg_prog = [ progname, input_file ]
+    
+    if not progarg:
+        arg_prog = [ progname ]
+        if not taint_stdin:
+            arg_prog.append(input_file)
+    else:
+        progarg = re.sub('\$input', input_file, progarg)
+        progarg = progarg.split()
+        arg_prog = [ progname ] + progarg
+    
+    if not taint_stdin:
+        stdin = None
+    else:
+        stdin = open(input_file, 'r')
 
-  
+    p = subprocess.Popen(arg_valgrind + arg_prog, stdin=stdin, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    p.wait()
+    #print "run over, wait for data process"
+
+    fp = open('massif-output', 'r')
+
+    heap1 = 0
+    heap2 = 0
+    total = 0
+    for line in fp:
+        line = line.strip()
+        
+        m1 = re.match('total=([\d]+)', line)
+        if m1:
+            total = int(m1.group(1))
+            break
+ 
+    #total = heap1 + heap2
+    fp.close()
+    print "total:",total
+    return total
+
+
+if __name__ == "__main__":
+    total = score('/usr/bin/convert', '/tmp/bla.jpg')
+    print total
